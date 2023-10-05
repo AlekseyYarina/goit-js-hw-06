c921959eb0f5160c8bc65adc50baa603e053cad9
AlekseyYarina <aleksey.yarina@gmail.com>
2023-10-05 22:29:01 +0300

    task2

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const categories = document.querySelector("#categories");
+const list = categories.querySelectorAll("li.item");
+const numberOfList = list.length;
+console.log(`Number of categories: ${numberOfList}`);
+
+list.forEach(item => {
+const title = item.querySelector("h2").textContent;
+const elements = item.querySelectorAll("li").length;
+console.log(`Category: ${title}`);
+console.log(`Elements: ${elements}`);
+})
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingrlist = document.querySelector("#ingredients");
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  ingrlist.appendChild(listItem);
+});
