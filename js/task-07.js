a5dd68a14489077b23b5e0b98fde3fca6422390c
AlekseyYarina <aleksey.yarina@gmail.com>
2023-10-11 13:49:39 +0300

    add task-07

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const inputSizeControl = document.querySelector("#font-size-control");
+const changeSizeText = document.querySelector("#text");
+
+inputSizeControl.addEventListener("input", changingSize);
+
+function changingSize(){
+    const fontSize = inputSizeControl.value + "px";
+    changeSizeText.style.fontSize = fontSize;
+}
