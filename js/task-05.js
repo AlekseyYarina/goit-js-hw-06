1886b13180b8b46bca0bdc3ab12f9fcd172d2f4e
AlekseyYarina <aleksey.yarina@gmail.com>
2023-10-11 22:40:04 +0300

    upd task-05

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,5 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 input.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value.trim();
+  output.textContent = event.currentTarget.value.trim() || "Anonymous";
 });
