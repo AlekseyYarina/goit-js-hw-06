a9aa5aa0599e807beb1c2c2fcfad437f25980b36
AlekseyYarina <aleksey.yarina@gmail.com>
2023-10-11 18:00:18 +0300

    Add task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widget = document.querySelector(".widget");
+const nameOfColor = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+
+btnChangeColor.addEventListener("click", cangeBackgroundColor);
+
+function cangeBackgroundColor(){
+  const newColor = getRandomHexColor();
+  widget.style.backgroundColor = newColor;
+  nameOfColor.textContent = newColor;
+}
