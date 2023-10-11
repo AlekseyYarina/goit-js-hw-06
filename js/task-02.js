a9bf1ba5c645bdc68300d3fa2fd038c77316cb1f
AlekseyYarina <aleksey.yarina@gmail.com>
2023-10-11 22:34:19 +0300

    upd task-02

--- js/task-02.js
+++ js/task-02.js
@@ -9,7 +9,7 @@ const ingredients = [
 
 const ingrList = document.querySelector("#ingredients");
 
-  const arrayList = ingredients.map(ingredient => {
-    return `<li class="item">${ingredient}</li>`;
-  });
-  ingrList.innerHTML = arrayList.join('');
+const arrayList = ingredients.map((ingredient) => {
+  return `<li class="item">${ingredient}</li>`;
+});
+ingrList.innerHTML = arrayList.join("");
