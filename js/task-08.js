50bd222a532a35943382ee5d874a5b64ea671665
AlekseyYarina <aleksey.yarina@gmail.com>
2023-10-11 15:54:43 +0300

    add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector(".login-form");
+const email = form.firstElementChild;
+const password = form.nextSibling;
+const button = form.lastElementChild;
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mailFormEl = formElements.email.value;
+  const passwordFormEl = formElements.password.value;
+
+  if (mailFormEl === "" || passwordFormEl === "") {
+    alert("You didn't enter email or password");
+  }
+
+  const formData = {
+    email: mailFormEl,
+    password: passwordFormEl,
+  };
+  console.log(formData);
+  
+  form.reset();
+}
